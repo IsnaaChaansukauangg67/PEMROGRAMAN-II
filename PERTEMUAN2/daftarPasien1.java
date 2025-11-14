302b89dfef2054a1bf091fce0b0c7de5d06483a1
IsnaaChaansukauangg67 <isnahamidg35@gmail.com>
2025-11-15 00:28:47 +0700

    Add files via upload

---
+++ PERTEMUAN2/daftarPasien1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Pert2_pengumpulan;
+
+/**
+ *
+ * @author isna
+ */
+public class daftarPasien1 {
+
+    public static void main(String[] args) {
+        Pasien1 P1 = new Pasien1 ("SUMANTO", 45,"M34","Kritis");
+        P1.daftarPasien();
+    
+    }
+}
