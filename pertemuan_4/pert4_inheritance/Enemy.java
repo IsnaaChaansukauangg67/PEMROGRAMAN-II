3e5240bd49dfbc6641848bcadb99c928e2ab3846
Isnaa Chaan <isnahamidg35@gmail.com>
2025-11-28 15:34:31 +0700

    Add files via upload

---
+++ pertemuan_4/pert4_inheritance/Enemy.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pert4_inheritance;
+
+/**
+ *
+ * @author isna
+ */
+class Enemy {
+    String name;
+    int hp;
+    int attackPoint;
+    
+    public void attack(){
+        System.out.println("Monster serang!");
+    }
+    
+}
